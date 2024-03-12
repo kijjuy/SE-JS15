cca465afe74309ecf65c8a8737d45240f50aa832
Brandon <brandonzomer@gmail.com>
2024-03-12 11:24:27 -0400

    migrations

---
+++ project/version1/dbms-mvc/Data/Migrations/20240312151432_AppUserAddDevices.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dbms_mvc.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppUserAddDevices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovedDeviced",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovedDeviced",
+                table: "AspNetUsers");
+        }
+    }
+}
