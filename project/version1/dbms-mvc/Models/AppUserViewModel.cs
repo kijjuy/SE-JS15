eac1644262ab2bf6d8b674f93c633d69383fd94f
Brandon <brandonzomer@gmail.com>
2024-03-12 12:03:57 -0400

    change roles to IList

--- project/version1/dbms-mvc/Models/AppUserViewModel.cs
+++ project/version1/dbms-mvc/Models/AppUserViewModel.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Identity;
 public class AppUserViewModel
 {
 
-    public AppUserViewModel(ApplicationUser appUser, List<string> roles)
+    public AppUserViewModel(ApplicationUser appUser, IList<string> roles)
     {
         this.ApprovedDevices = appUser.ApprovedDeviced;
         this.Email = appUser.Email;
@@ -15,6 +15,6 @@ public class AppUserViewModel
 
     public string Email { get; set; }
 
-    public List<string>? Roles { get; set; }
+    public IList<string>? Roles { get; set; }
 
 }
