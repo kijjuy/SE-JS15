edc80d3a5012917e9daa0104e398c6a79851c511
kijjuy <brandonzomer@proton.me>
2024-04-02 14:24:30 -0400

    add RegistrationCode prop

--- project/version1/dbms-mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ project/version1/dbms-mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -67,6 +67,10 @@ namespace dbms_mvc.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Display(Name = "Registration Code")]
+            public string RegistrationCode { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
