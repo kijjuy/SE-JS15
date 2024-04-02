64c3297c0fc44137d3534b80c613afef49b5d5a8
kijjuy <brandonzomer@proton.me>
2024-04-02 19:29:57 -0400

    add primary key

--- project/version1/dbms-mvc/Models/RegistrationCode.cs
+++ project/version1/dbms-mvc/Models/RegistrationCode.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 
 public class RegistrationCode
 {
+    [Key]
+    public int RegistrationCodeId { get; set; }
+
     public Guid Token { get; set; }
+
     public DateTime Expiration { get; set; }
 
     public RegistrationCode()
