527c0a79d309c1e582ddc21ec2211559bfe2d8ff
kijjuy <brandonzomer@proton.me>
2024-04-10 14:45:43 -0400

    create ContactsRepositoryTests, add AddUser() test

---
+++ project/version1/dbms-mvc.Tests/Repositories/ContactsRepositoryTests.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using AutoFixture;
+using dbms_mvc.Repositories;
+using dbms_mvc.Data;
+
+namespace dbms_mvc.Tests.Repositories;
+
+[TestClass]
+public class ContactsRepositoryTests
+{
+    private ApplicationDbContext _context;
+    private Fixture _fixture;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        DbContextOptionsBuilder<ApplicationDbContext> dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseInMemoryDatabase(
+        Guid.NewGuid().ToString()
+        );
+
+        _context = new ApplicationDbContext(dbOptions.Options);
+
+        _fixture = new Fixture();
+    }
+
+    public async Task AddContact()
+    {
+        //Arrange
+        IContactsRepository repository = new ContactsRepository(_context);
+
+        Contact testContact = _fixture.Create<Contact>();
+
+
+        //Act
+        await repository.AddContact(testContact);
+
+        //Assert
+        Contact dbContact = await _context.contacts.FirstOrDefaultAsync();
+        Assert.Equals(testContact, dbContact);
+    }
+}
