0439aec43a2082e4b974add6631fa7fce2a84673
kijjuy <brandonzomer@proton.me>
2024-04-10 13:41:44 -0400

    add GetUploadMergeConflicts method

--- project/version1/dbms-mvc/Repositories/Interfaces/IContactsRepository.cs
+++ project/version1/dbms-mvc/Repositories/Interfaces/IContactsRepository.cs
@@ -1,14 +1,15 @@
+using dbms_mvc.Models;
+
 namespace dbms_mvc.Repositories;
 
 public interface IContactsRepository : IDisposable
 {
     public Task<IEnumerable<Contact>> SearchContacts(Contact searchContact);
 
+    public Task<IEnumerable<MergeConflictViewModel>> GetUploadMergeConflicts(IEnumerable<Contact> newContacts);
+
     public Task<Contact?> GetContactById(int? id);
-
     public Task AddContact(Contact contact);
-
     public Task UpdateContact(Contact contact);
-
     public Task DeleteContact(Contact contact);
 }
