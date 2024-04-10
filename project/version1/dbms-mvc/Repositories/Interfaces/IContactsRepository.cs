9b9f36aaeb894187b4a6df1ae461565550fea58b
kijjuy <brandonzomer@proton.me>
2024-04-10 12:47:12 -0400

    add GetContactById abstract method

--- project/version1/dbms-mvc/Repositories/Interfaces/IContactsRepository.cs
+++ project/version1/dbms-mvc/Repositories/Interfaces/IContactsRepository.cs
@@ -1,7 +1,8 @@
-using dbms_mvc.Models;
 namespace dbms_mvc.Repositories;
 
 public interface IContactsRepository : IDisposable
 {
     public Task<IEnumerable<Contact>> SearchContacts(Contact searchContact);
+
+    public Task<Contact?> GetContactById(int? id);
 }
