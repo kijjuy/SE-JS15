63915ab9456aa229f519eb17f7a06a3e6649580f
kijjuy <brandonzomer@proton.me>
2024-04-11 13:56:41 -0400

    create SpreadsheetService implementation

---
+++ project/version1/dbms-mvc/Services/Implementations/SpreadsheetService.cs
@@ -0,0 +1,19 @@
+namespace dbms_mvc.Services;
+
+/// <summary>
+/// Implementation of ISpreadsheetService
+/// </summary>
+public class SpreadsheetService : ISpreadsheetService
+{
+    private readonly ILogger<SpreadsheetService> _logger;
+
+    public SpreadsheetService(ILogger<SpreadsheetService> logger)
+    {
+        _logger = logger;
+    }
+
+    public IEnumerable<Contact> GetContactsFromFile(IFormFile file)
+    {
+        throw new NotImplementedException();
+    }
+}
