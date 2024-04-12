491756b3533a5bcc8926513e4208a6a9f52e5706
kijjuy <brandonzomer@proton.me>
2024-04-12 02:01:26 -0400

    add SpreadsheetService tests

---
+++ project/version1/dbms-mvc.Tests/Services/SpreadsheetServiceTests.cs
@@ -0,0 +1,26 @@
+using AutoFixture;
+using dbms_mvc.Services;
+
+namespace dbms_mvc.Tests.Services;
+
+[TestClass]
+public class SpreadsheetServiceTests
+{
+    private readonly ISpreadsheetService _spreadsheetService;
+    private readonly IFixture _fixture;
+
+    public SpreadsheetServiceTests()
+    {
+        _spreadsheetService = new SpreadsheetService(null);
+
+        _fixture = new Fixture();
+    }
+
+    [TestMethod]
+    public void GetContactsFromFile()
+    {
+        //Arrange
+        var contacts = _fixture.CreateMany<Contact>();
+        var contact = _fixture.Create<Contact>();
+    }
+}
