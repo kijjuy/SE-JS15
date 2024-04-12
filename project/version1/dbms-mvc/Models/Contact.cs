9e56bfe38becd197f207624416407bdbf2043f39
kijjuy <brandonzomer@proton.me>
2024-04-12 01:58:36 -0400

    add SpreadsheetColumn attribute to model items

--- project/version1/dbms-mvc/Models/Contact.cs
+++ project/version1/dbms-mvc/Models/Contact.cs
@@ -42,64 +42,82 @@ public class Contact
 
     [Required]
     [StringLength(25)]
+    [SpreadsheetColumn("First")]
     public string FirstName { get; set; }
 
     [Required]
     [StringLength(25)]
+    [SpreadsheetColumn("Name")]
     public string LastName { get; set; }
 
     [Required]
     [StringLength(50)]
+    [SpreadsheetColumn("Organization")]
     public string Organization { get; set; }
 
     [Required]
     [StringLength(25)]
+    [SpreadsheetColumn("Title")]
     public string Title { get; set; }
 
     [Required]
     [StringLength(50)]
+    [SpreadsheetColumn("Address")]
     public string StreetAddress1 { get; set; }
 
     [Required]
     [StringLength(25)]
+    [SpreadsheetColumn("City")]
     public string City { get; set; }
 
     [Required]
     [StringLength(2)]
+    [SpreadsheetColumn("Province")]
     public string Province { get; set; }
 
     [Required]
     [StringLength(6)]
+    [SpreadsheetColumn("Postal Code")]
     public string PostalCode { get; set; }
 
     [Required]
     [StringLength(1)]
+    [SpreadsheetColumn("Subscribed Y/N")]
     public string Subscribed { get; set; }
 
     [Required]
     [StringLength(50)]
+    [SpreadsheetColumn("Emails")]
     public string Email { get; set; }
 
     [Required]
     [StringLength(11)]
+    [SpreadsheetColumn("Phone")]
     public string Phone { get; set; }
 
+    [SpreadsheetColumn("Home Category")]
     public string? HomeCategory { get; set; }
 
     [StringLength(11)]
+    [SpreadsheetColumn("Fax")]
     public string? Fax { get; set; }
 
     [StringLength(50)]
+    [SpreadsheetColumn("Website")]
     public string? Website { get; set; }
 
+    [SpreadsheetColumn("Number of Beds")]
     public int? BedsCount { get; set; }
 
     [StringLength(50)]
+    [SpreadsheetColumn("Address 2")]
     public string? Address2 { get; set; }
 
     [StringLength(4)]
+    [SpreadsheetColumn("Extension")]
     public string? Extension { get; set; }
 
     [Required]
+    [SpreadsheetColumn("Mailing List")]
     public string MailingList { get; set; }
 }
