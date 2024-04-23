ee0fba103d1ac4cb6cc169b07eb03d912f4373f2
kijjuy <brandonzomer@proton.me>
2024-04-23 15:01:31 -0400

    change error view model and page

--- dbms-mvc/Models/ViewModels/ErrorViewModel.cs
+++ dbms-mvc/Models/ViewModels/ErrorViewModel.cs
@@ -5,4 +5,16 @@ public class ErrorViewModel
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    public string? Message { get; set; }
+
+    public bool ShowMessage => !string.IsNullOrEmpty(Message);
+
+    public ErrorViewModel() { }
+
+    public ErrorViewModel(string requestId, string message)
+    {
+        this.RequestId = requestId;
+        this.Message = message;
+    }
 }
